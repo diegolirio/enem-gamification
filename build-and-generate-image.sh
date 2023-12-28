668210bfa5d7109971fe3497ea17a2783eda060a
Diego Lírio <diegolirio.dl@gmail.com>
2023-12-28 09:27:41 -0300

    Update build-and-generate-image.sh

--- build-and-generate-image.sh
+++ build-and-generate-image.sh
@@ -1,18 +1,24 @@
 
 current_version=$(awk -F\" '/version =/ {print $2}' build.gradle.kts)
 
+echo 'Generating new version'
 new_version=$(echo "$current_version" | awk -F. '{$NF+=1; OFS="."; print $0}' OFS='.')
 new_version_with_suffix="$new_version-SNAPSHOT"
 
-# changing version in the build.gralde.kts
+echo 'Upgrading version in the build.gralde.kts'
 awk -v old="$current_version" -v new="$new_version_with_suffix" '{sub(old, new)}1' build.gradle.kts > build.gradle.kts.tmp && mv build.gradle.kts.tmp build.gradle.kts
 
 echo 'Old version: ' $current_version
 echo 'New version: ' $new_version_with_suffix
 
+echo "Building new image | version $new_version_with_suffix"
 ./gradlew :app:bootBuildImage
 
-docker tag diegolirio/enem-gamification-app:0.0.1-SNAPSHOT diegolirio/enem-gamification-app:latest
+echo 'Taging for latest'
+docker tag diegolirio/enem-gamification-app:$new_version_with_suffix diegolirio/enem-gamification-app:latest
 
-docker push diegolirio/enem-gamification-app:0.0.1-SNAPSHOT
+echo 'Pushing new version '$new_version_with_suffix
+docker push diegolirio/enem-gamification-app:$new_version_with_suffix
+
+echo 'Pushing latest version '
 docker push diegolirio/enem-gamification-app:latest
